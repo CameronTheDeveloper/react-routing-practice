777e7c8deb5cca3d39ce2f6dcb7ddc78f97e7b94
Cameron Posey <cameronposey@outlook.com>
2024-01-15 21:41:35 -0800

    Add ErrorPage a errorElement

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import Profile from "./components/Profile";
+import ErrorPage from "./components/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />,
   },
   {
     path: "profile/:name",
